f066befe7ffa5416d9a9a3c927f22eb4d4040b51
Hamza Munir <infoathamza@gmail.com>
2026-04-05 07:55:30 +0500

    feat: finalize app layout and remove unused FeaturesGrid

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,33 @@ import Footer from "./components/layout/Footer";
 import AdPlaceholder from "./components/layout/AdPlaceholder";
 import HeroGenerator from "./components/sections/HeroGenerator";
 import HowItWorks from "./components/sections/HowItWorks";
-import FeaturesGrid from "./components/sections/FeaturesGrid";
 import FAQ from "./components/sections/FAQ";
 import GeneratorWidget from "./components/generator/GeneratorWidget";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-background">
       <Navbar />
+      
       <main>
+        {/* Hero & Generator */}
         <HeroGenerator>
           <GeneratorWidget />
         </HeroGenerator>
         
+        {/* Top Ad Slot */}
         <AdPlaceholder className="!my-0 !border-t-0" />
         
+        {/* Simple Steps Section */}
         <HowItWorks />
-        <FeaturesGrid />
+        
+        {/* FAQ Section */}
         <FAQ />
         
+        {/* Bottom Ad Slot */}
         <AdPlaceholder className="!my-0 border-b-0" />
       </main>
+
       <Footer />
     </div>
   );
